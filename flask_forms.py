09fde00760b2ba2af0954d23b8dc0c4dd72dfab3
bunsanandme <bunsanandme@yandex.ru>
2022-04-11 03:26:21 +0300

    preparing for host

--- flask_forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, DateField
-from wtforms.validators import DataRequired
-
-
-class ClientForm(FlaskForm):
-    email = StringField(validators=[DataRequired()])
-    sub = SubmitField("Получить")
-
