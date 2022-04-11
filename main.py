439aea2835389a4dba66c242c16a8cd3d94023a1
bunsanandme <bunsanandme@yandex.ru>
2022-04-11 03:58:18 +0300

    fix

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ import sql
 import time
 
 if __name__ == "__main__":
-    while True:
-        some_email = "".join(imap.get_headers_email(1, "E_ONLY"))
-        print("Полученный адрес: ",some_email)
-        if sql.get_client_data(some_email):
-            smtp.send_message_clients(some_email)
-        time.sleep(600)
+        for i in sql.query_executor_select("SELECT Email FROM Clients"):
+                smtp.send_message_meetings(i[0])
+
+        while True:
+                some_email = "".join(imap.get_headers_email(1, "E_ONLY"))
+                print("Полученный адрес: ",some_email)
+                if sql.get_client_data(some_email):
+                    smtp.send_message_clients(some_email)
+                print("Ожидание: 10 минут...zzz")
+                time.sleep(600)
